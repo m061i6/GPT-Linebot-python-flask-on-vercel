89233e2fb41d36453fa57d0e712ebecc15097c77
m061i6 <oo00oo910@gmail.com>
2022-12-15 01:22:45 +0800

    change to 公威

--- api/index.py
+++ api/index.py
@@ -39,7 +39,7 @@ def handle_message(event):
     if event.message.type != "text":
         return
 
-    if event.message.text == "說話":
+    if event.message.text == "公威阿":
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
